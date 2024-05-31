4263f524f5d575e058863d3101d00cf675ff2755
Lucas Henrique <lucashcolivera@gmail.com>
2024-05-31 13:01:56 -0300

    implementado o inicio da edição de contatos e toast de erro ao localizar contato

--- packages/frontend/src/pages/EditContact/index.js
+++ packages/frontend/src/pages/EditContact/index.js
@@ -1,15 +1,50 @@
+import { useParams, useHistory } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import Loader from '../../components/Loader';
+import toast from '../../utils/toast';
+
+import ContactsService from '../../services/ContactsService';
 
 export default function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+  const history = useHistory();
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function loaadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(
+          id,
+        );
+        console.log({ contactData });
+        setIsLoading(false);
+      } catch {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado!',
+        });
+      }
+    }
+    loaadContact();
+  }, [id, history]);
+
+  function handleSubmit() {
+    //
+  }
   return (
     <>
+      <Loader isLoading={isLoading} />
       <PageHeader
         title="Editar Lucas Henrique"
       />
 
       <ContactForm
         buttonLabel="Salvar Alterações "
+        onSubmit={handleSubmit}
       />
 
     </>
