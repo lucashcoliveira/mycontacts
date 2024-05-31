9ea1939884fd7196a61a91efeb205abf7a51034e
Lucas Henrique <lucashcolivera@gmail.com>
2024-05-31 11:06:57 -0300

    adicionado o setTimmeout para remoção automatica do toast

--- packages/frontend/src/components/Toast/ToastMessage/index.js
+++ packages/frontend/src/components/Toast/ToastMessage/index.js
@@ -1,15 +1,25 @@
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 import { Container } from './styles';
 
 import xCircleIcon from '../../../assets/images/icons/x-circle.svg';
 import checkCircleIcon from '../../../assets/images/icons/check-circle.svg';
 
-export default function ToastMessaaaage({
-  message, onRemoveMessage,
-}) {
+export default function ToastMessaaaage({ message, onRemoveMessage }) {
+  useEffect(() => {
+    const timeOutIdd = setTimeout(() => {
+      onRemoveMessage(message.id);
+    }, message.duration || 7000);
+
+    return () => {
+      clearTimeout(timeOutIdd);
+    };
+  }, [message, onRemoveMessage]);
+
   function handleRemoveToast() {
     onRemoveMessage(message.id);
   }
+
   return (
     <Container
       type={message.type}
@@ -29,6 +39,7 @@ ToastMessaaaage.propTypes = {
     id: PropTypes.number.isRequired,
     text: PropTypes.string.isRequired,
     type: PropTypes.oneOf(['defalut', 'success', 'danger']),
+    duration: PropTypes.number,
   }).isRequired,
 
   onRemoveMessage: PropTypes.func.isRequired,
