381301a31482574989cf83f35e3fca94323a6acb
Lucas Henrique <lucashcolivera@gmail.com>
2024-07-22 12:34:06 -0300

    inicio da refatoração do componente useAnimatedList

--- packages/frontend/src/hooks/useAnimatedList.js
+++ packages/frontend/src/hooks/useAnimatedList.js
@@ -1,9 +1,13 @@
-import { useCallback, useState } from 'react';
+import {
+  createRef, useCallback, useRef, useState,
+} from 'react';
 
 export default function useAnimatedList(initialValue = []) {
   const [items, setItems] = useState(initialValue);
 
   const [pendingRemovalItemsIds, setPendingRemovalItemsIds] = useState([]);
+
+  const animatedRefs = useRef(new Map());
 
   const handleRemoveItem = useCallback((id) => {
     setPendingRemovalItemsIds(
@@ -11,24 +15,35 @@ export default function useAnimatedList(initialValue = []) {
     );
   }, []);
 
-  const handleAnimatonEnd = useCallback((id) => {
-    setItems((prevState) => prevState.filter((item) => item.id !== id));
-    setPendingRemovalItemsIds(
-      (prevState) => prevState.filter((itemId) => itemId !== id),
-    );
+  const getAnimatedRef = useCallback((itemID) => {
+    let animatedRef = animatedRefs.current.get(itemID);
+
+    if (!animatedRef) {
+      animatedRef = createRef();
+      animatedRefs.current.set(itemID, animatedRef);
+    }
+    return animatedRef;
   }, []);
 
-  const renderList = useCallback((renderItem) => (
-    items.map((item) => renderItem(item, {
-      isLeaving: pendingRemovalItemsIds.includes(item.id),
-    }))
-  ), [items, pendingRemovalItemsIds]);
+  // const handleAnimatonEnd = useCallback((id) => {
+  //   setItems((prevState) => prevState.filter((item) => item.id !== id));
+  //   setPendingRemovalItemsIds(
+  //     (prevState) => prevState.filter((itemId) => itemId !== id),
+  //   );
+  // }, []);
 
+  const renderList = useCallback((renderItem) => (
+    items.map((item) => {
+      const isLeaving = pendingRemovalItemsIds.includes(item.id);
+      const animatedRef = getAnimatedRef(item.id);
+
+      return renderItem(item, { isLeaving, animatedRef });
+    })
+  ), [items, pendingRemovalItemsIds, getAnimatedRef]);
   return {
     items,
     setItems,
     handleRemoveItem,
-    handleAnimatonEnd,
     renderList,
   };
 }
